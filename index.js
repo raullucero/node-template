ee6987e7940312b366d47d136219c8f118a8d256
Raul Lucero <raul.lucero27@gmail.com>
2016-11-30 04:41:24 -0600

    conection with the db and initializing morgan too

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const path = require('path');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+
+const routes = require('./api/routes/index');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost/node');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongo is connected');
+});
 // Configuration
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
 
 app.use('/', routes);
 
