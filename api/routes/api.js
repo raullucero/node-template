776273c9ed651394b8756924299fcaf5ee969dcb
Raul Lucero <raul.lucero27@gmail.com>
2016-12-02 13:23:46 -0600

    add routes reference on api for urls

--- api/routes/api.js
+++ api/routes/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const users = require('../controllers/users/users.route');
+const urls = require('../controllers/url/url.route');
 
 router.use('/users', users);
+
+router.use('/urls', urls);
 
 router.get('/login', (req, res) => {
   res.send('from the login callback');
